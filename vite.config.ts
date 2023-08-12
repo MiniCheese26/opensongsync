f2171fe62be6e6ad69562e934533d592569a8808
Emily Medhurst <emily.medhurst2002@protonmail.com>
2023-08-12 12:58:45 +0100

    use env variables in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
-export default defineConfig({
-	server: {
-		https: true,
-		proxy: {
-			'/api/jobs/': 'http://localhost:3000'
-		}
-	},
-	plugins: [
-		sveltekit(),
-		mkcert({
-			hosts: ['opensongsync.dev']
-		})
-	]
+export default defineConfig(({ mode }) => {
+	Object.assign(process.env, loadEnv(mode, process.cwd(), 'PUBLIC'));
+
+	let host = process.env.PUBLIC_DOMAIN;
+
+	if (host) {
+		host = host.split(':')[0];
+	}
+	
+	return {
+		server: {
+			https: process.env.PUBLIC_HTTPS?.toLocaleLowerCase() === 'true',
+			proxy: {
+				'/api/jobs/': 'http://localhost:3000'
+			}
+		},
+		plugins: [
+			sveltekit(),
+			mkcert({
+				hosts: host ? [host] : undefined
+			})
+		]
+	};
 });
